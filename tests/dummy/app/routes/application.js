a21fe7826674ea6df74dacf4f9ff77d4ae538751
Randall Morey <randall@randallmorey.com>
2023-02-27 13:09:03 -0500

    fix: use relative file path

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
   async model() {
-    const asciicast = await fetch('/example.cast');
+    const asciicast = await fetch('example.cast');
     const asciicastContent = await asciicast.text();
     return asciicastContent;
   }
